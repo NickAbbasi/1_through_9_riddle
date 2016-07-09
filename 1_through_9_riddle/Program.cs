55a1bc52ef4b026088527b1be89efd27b820b278
NickAbbasi <nickabbasi1@gmail.com>
2016-07-09 19:08:51 -0400

    first

---
+++ 1_through_9_riddle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_through_9_riddle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = "";
+
+            int a, b, c, d, e, f, g, h, i, x;
+
+            a = 1;
+            b = 2;
+            c = 3;
+            d = 4;
+            e = 5;
+            f = 6;
+            g = 7;
+            h = 8;
+            i = 9;
+            x = 10;
+
+
+            while (x < 10)
+            {
+
+                int n = a;
+                text
+
+
+            }
+
+
+        }
+    }
+}
